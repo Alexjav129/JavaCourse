db0907bf2fb2de49754979745b0636b9eece60f7
Alejandro J. Bautista <78135846+Alexjav129@users.noreply.github.com>
2024-02-11 19:27:55 -0600

    Create PrimitivosFloat.java

---
+++ HelloWorld/src/PrimitivosFloat.java
@@ -0,0 +1,12 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 2.12e3f; //2120f;
+        System.out.println("realFloat = " + realFloat);
+
+
+        float realFloat2 = 0.00000000015f; //1.5e-10f;
+        System.out.println("realFloat2 = " + realFloat2);
+
+    }
+}
